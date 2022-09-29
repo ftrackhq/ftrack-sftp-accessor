4f2d6b4ea450853d2cd8678d54dc866036947cc1
Ian Wootten <3598161+iwootten@users.noreply.github.com>
2022-09-29 16:51:32 +0100

    Use session.ensure for location lookup

--- scripts/sftp_download.py
+++ scripts/sftp_download.py
@@ -11,22 +11,23 @@ from ftrack_sftp_accessor.sftp import SFTPAccessor
 Copies a component from a sftp location to a local location
 """
 session = ftrack_api.Session()
-sftp_location = session.query('Location where name is "studio.sftp"').one()
+sftp_location = session.ensure('Location', {"name":"studio.sftp"})
 
 hostname = os.getenv("FTRACK_SFTP_ACCESSOR_HOSTNAME", None)
+username = os.getenv("FTRACK_SFTP_ACCESSOR_USERNAME", None)
 port = os.getenv("FTRACK_SFTP_ACCESSOR_PORT", 22)
 
-sftp_location.accessor = SFTPAccessor(hostname, port=port)
+sftp_location.accessor = SFTPAccessor(hostname, username, port=port)
 sftp_location.structure = ftrack_api.structure.standard.StandardStructure()
 sftp_location.priority = 30
 
-ftrack_location = session.query('Location where name is "ftrack.connect"').one()
+ftrack_location = session.ensure('Location', {"name":"ftrack.connect"})
 ftrack_location.structure = ftrack_api.structure.standard.StandardStructure()
 ftrack_location.accessor = ftrack_api.accessor.disk.DiskAccessor(
     prefix="/Users/ian/ftrack_storage"
 )
 
-version = session.get("AssetVersion", "b1ddbf94-6807-4f9f-a404-02298df80bba")
+version = session.get("AssetVersion", "9d5d86b7-46b5-49fc-993c-03ce885a6950")
 
 for component in version["components"]:
     if component["name"] == "main":
--- scripts/sftp_upload.py
+++ scripts/sftp_upload.py
@@ -10,14 +10,15 @@ session = ftrack_api.Session()
 location = session.query('Location where name is "studio.sftp"').one()
 
 hostname = os.getenv("FTRACK_SFTP_ACCESSOR_HOSTNAME", None)
+username = os.getenv("FTRACK_SFTP_ACCESSOR_USERNAME", None)
 port = os.getenv("FTRACK_SFTP_ACCESSOR_PORT", 22)
 
 # Setup accessor
-location.accessor = SFTPAccessor(hostname, port=port)
+location.accessor = SFTPAccessor(hostname, username, port=port)
 location.structure = ftrack_api.structure.standard.StandardStructure()
 location.priority = 30
 
-version = session.get("AssetVersion", "87d07110-962b-11ea-8647-927195c3dfa3")
+version = session.get("AssetVersion", "83b88aa2-b739-4ed1-b840-2ccdac7da271")
 version.create_component(
     path="/Users/ian/Downloads/film0001_512kb.mp4", location=location
 )
